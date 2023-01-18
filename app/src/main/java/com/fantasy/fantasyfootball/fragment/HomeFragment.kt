52fd20cc9dd6ae2bf333fbbe94d8f7908991d4f7
Xiang Ze <xchuay@gmail.com>
2023-01-18 14:28:00 +0800

    added pick team layout plus home

--- app/src/main/java/com/fantasy/fantasyfootball/fragment/HomeFragment.kt
+++ app/src/main/java/com/fantasy/fantasyfootball/fragment/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.fragment.NavHostFragment
 import com.fantasy.fantasyfootball.R
 import com.fantasy.fantasyfootball.databinding.FragmentHomeBinding
 
@@ -17,5 +20,14 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnPickTeam.setOnClickListener {
+           val action = HomeFragmentDirections.actionHomeFragmentToPickTeamFragment()
+           NavHostFragment.findNavController(this).navigate(action)
+        }
     }
 }
--- app/src/main/java/com/fantasy/fantasyfootball/fragment/PickPlayerFragment.kt
+++ app/src/main/java/com/fantasy/fantasyfootball/fragment/PickPlayerFragment.kt
@@ -6,13 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.fantasy.fantasyfootball.R
+import com.fantasy.fantasyfootball.databinding.FragmentHomeBinding
+import com.fantasy.fantasyfootball.databinding.FragmentPickPlayerBinding
 
 class PickPlayerFragment : Fragment() {
+    private lateinit var binding: FragmentPickPlayerBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pick_player, container, false)
+        binding = FragmentPickPlayerBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
