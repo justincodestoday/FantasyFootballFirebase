a058166099ffa725bfcadaf6879e629aa641f219
Justin <jcmc888@gmail.com>
2023-01-29 03:41:46 +0800

    added snackbar and toast to register

--- app/src/main/java/com/fantasy/fantasyfootball/data/model/User.kt
+++ app/src/main/java/com/fantasy/fantasyfootball/data/model/User.kt
@@ -1,5 +1,6 @@
 package com.fantasy.fantasyfootball.data.model
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -9,8 +10,4 @@ data class User(
     var name: String? = null,
     var username: String? = null,
     var password: String? = null,
-//    val teamName: String? = null,
-//    var points: Int = 0,
-//    var remainingBudget: Float = 0f,
-//    var lastUpdated: Date? = null,
 )
--- app/src/main/java/com/fantasy/fantasyfootball/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/fantasy/fantasyfootball/viewModel/RegisterViewModel.kt
@@ -1,10 +1,7 @@
 package com.fantasy.fantasyfootball.viewModel
 
-import androidx.databinding.ObservableArrayList
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.databinding.ObservableField
+import androidx.lifecycle.*
 import com.fantasy.fantasyfootball.R
 import com.fantasy.fantasyfootball.constant.Enums
 import com.fantasy.fantasyfootball.data.model.User
@@ -21,6 +18,8 @@ class RegisterViewModel(private val repo: UserRepository) : ViewModel() {
 
     val success: MutableSharedFlow<String> = MutableSharedFlow()
     val error: MutableSharedFlow<String> = MutableSharedFlow()
+
+    val navigate: MutableSharedFlow<Unit> = MutableSharedFlow()
 
 //    val formErrors = ObservableArrayList<Enums.FormErrors>()
 
@@ -42,6 +41,64 @@ class RegisterViewModel(private val repo: UserRepository) : ViewModel() {
 //        return true
 //    }
 
+    val nameError = MediatorLiveData<String>().apply {
+        value = ""
+        addSource(name) {
+            value = validateName(it)
+        }
+    }
+
+    val usernameError = MediatorLiveData<String>().apply {
+        value = ""
+        addSource(username) {
+            value = validateUsername(it)
+        }
+    }
+
+//    val passwordError = MediatorLiveData<String>().apply {
+//        value = ""
+//        addSource(password) {
+//            value = validatePassword(it)
+//        }
+//    }
+
+    val passwordError = ObservableField<String>()
+
+    fun btnClick() {
+        if (password.value?.isEmpty() == true) {
+            passwordError.set("Empty password")
+            return
+        }
+
+        if (password.value?.length!! < 7) {
+            passwordError.set("Invalid pass")
+        } else {
+            passwordError.set(null)
+        }
+    }
+
+    fun validateName(name: String?): String? {
+        return when {
+            name.isNullOrEmpty() -> "Invalid name"
+            else -> null
+        }
+    }
+
+    fun validateUsername(username: String?): String? {
+        return when {
+            username.isNullOrEmpty() -> "Invalid username"
+            else -> null
+        }
+    }
+
+    fun validatePassword(password: String?): String? {
+        return when {
+            password.isNullOrEmpty() -> "Invalid password"
+            password.length < 8 -> "Password too short"
+            else -> null
+        }
+    }
+
     private fun validate(name: MutableLiveData<String>): Boolean {
         if (name.value.isNullOrEmpty()) {
             return false
@@ -52,13 +109,24 @@ class RegisterViewModel(private val repo: UserRepository) : ViewModel() {
     fun register() {
         viewModelScope.launch {
             val user =
-                User(name = name.value, username = username.value, password = password.value)
+                User(name = name.value?.trim(), username = username.value?.trim(), password = password.value?.trim())
+//            if (password.value?.isEmpty() == true) {
+//                passwordError.set("Empty password")
+//                return@launch
+//            }
+//
+//            if (password.value?.length!! < 7) {
+//                passwordError.set("Invalid pass")
+//            } else {
+//                passwordError.set(null)
+//            }
 //            repo.createUser(user)
-            if (!name.value.isNullOrEmpty() || !username.value.isNullOrEmpty() || !password.value.isNullOrEmpty() || passwordConfirm.value.isNullOrEmpty()) {
-                error.emit("Not filled")
+            if (name.value.isNullOrEmpty() || username.value.isNullOrEmpty() || password.value.isNullOrEmpty() || passwordConfirm.value.isNullOrEmpty()) {
+                error.emit(Enums.FormErrors.MISSING_NAME.name)
             } else {
                 repo.createUser(user)
-                success.emit(R.string.register_successfully.toString())
+                success.emit(Enums.FormSuccess.REGISTER_SUCCESSFUL.name)
+//                navigate.emit(Unit)
             }
         }
     }
