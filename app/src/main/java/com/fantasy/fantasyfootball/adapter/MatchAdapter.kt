27e925647b593fe3ef66d4e54ff7f76a7a8010e9
Xiang Ze <xchuay@gmail.com>
2023-02-08 15:40:16 +0800

    added matches design

--- app/src/main/java/com/fantasy/fantasyfootball/adapter/MatchAdapter.kt
+++ app/src/main/java/com/fantasy/fantasyfootball/adapter/MatchAdapter.kt
@@ -1,7 +1,9 @@
 package com.fantasy.fantasyfootball.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.fantasy.fantasyfootball.data.model.Matches
 import com.fantasy.fantasyfootball.databinding.MatchCardBinding
@@ -22,6 +24,35 @@ class MatchAdapter(var matches: List<Matches>):
             tvHomeScore.text = match.homeScore.toString()
             tvAwayScore.text = match.awayScore.toString()
             tvDate.text = match.date
+            when (match.homeTeam.name) {
+                "Arsenal" -> {
+                    arsenalHome.isVisible = true
+                }
+                "Chelsea" -> {
+                    chelseaHome.isVisible = true
+                }
+                "Liverpool" -> {
+                    liverpoolHome.isVisible = true
+                }
+                "ManUnited" -> {
+                    manutdHome.isVisible = true
+                }
+            }
+
+            when (match.awayTeam.name) {
+                "Arsenal" -> {
+                    arsenalAway.isVisible = true
+                }
+                "Chelsea" -> {
+                    chelseaAway.isVisible = true
+                }
+                "Liverpool" -> {
+                    liverpoolAway.isVisible = true
+                }
+                "ManUnited" -> {
+                    manutdAway.isVisible = true
+                }
+            }
             cvMatchCard.setOnClickListener {
             }
         }
