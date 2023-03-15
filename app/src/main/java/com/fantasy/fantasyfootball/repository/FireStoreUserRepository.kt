54d9ad08dce360126652c642512fbf0e5b619a3e
Xiang Ze <xchuay@gmail.com>
2023-03-15 20:19:22 +0800

    fixing user repo

--- app/src/main/java/com/fantasy/fantasyfootball/repository/FireStoreUserRepository.kt
+++ app/src/main/java/com/fantasy/fantasyfootball/repository/FireStoreUserRepository.kt
@@ -1,53 +1,60 @@
 package com.fantasy.fantasyfootball.repository
 
 import com.fantasy.fantasyfootball.data.model.User
-import com.fantasy.fantasyfootball.data.model.UserWithTeam
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.tasks.await
 
-class FireStoreUserRepository(private val auth: FirebaseAuth, private val ref: CollectionReference):
+class FireStoreUserRepository(
+    private val auth: FirebaseAuth,
+    private val ref: CollectionReference
+) :
     UserRepository {
-    override suspend fun getUserById(userId: Int): Flow<User?> {
-        TODO("Not yet implemented")
+    override suspend fun updateUser(id: String, user: User) {
+        val email = auth.currentUser?.email
+        var docId = "testId"
+        val query = ref.whereEqualTo("email", email).get().await()
+        query.documents.forEach {
+            docId = it.id
+        }
+        val doc = ref.document(docId)
+        doc.set(user).await()
     }
 
-    override suspend fun getUserByUsername(username: String): User? {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getUserCredentials(username: String, password: String): User? {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun createUser(user: User): Long {
-        val res = auth.createUserWithEmailAndPassword(user.username!!, user.password!!).await()
+    override suspend fun register(user: User): FirebaseUser? {
+        val res = auth.createUserWithEmailAndPassword(user.email!!, user.password!!).await()
 
         if (res.user != null) {
-            ref.document(user.username).set(user).await()
+//            ref.document(user.email).set(user).await()
+            ref.add(user).await()
         }
+
+        return res.user
     }
 
-    override suspend fun editUser(id: Int, user: User): Long {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getUsersWithTeams(): List<UserWithTeam> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getUserWithTeam(userId: Int): UserWithTeam? {
-        TODO("Not yet implemented")
-    }
-
-    suspend fun login(email: String, password: String): Boolean? {
+    override suspend fun login(email: String, password: String): Boolean {
         val res = auth.signInWithEmailAndPassword(email, password).await()
+
         return res.user?.uid != null
     }
 
-    fun isAuthenticate(): Boolean {
-        val user = auth.currentUser ?: return false
+    fun getUid(): String? {
+        return auth.uid
+    }
+
+    suspend fun fetchUser(user: User): FirebaseUser? {
+        val res = auth.createUserWithEmailAndPassword(user.email!!, user.password!!).await()
+
+        return res.user
+    }
+
+    fun isAuthenticated(): Boolean {
+        val user = auth.currentUser
+        if (user == null) {
+            return false
+        }
         return true
     }
 
@@ -55,10 +62,20 @@ class FireStoreUserRepository(private val auth: FirebaseAuth, private val ref: C
         auth.signOut()
     }
 
-    suspend fun getCurrentUser(): User? {
-        return auth.currentUser?.email?.let {
-            ref.document(it).get().await().toObject(User::class.java)
-        }
-    }
+//    suspend fun getCurrentUser(): User? {
+//        // this one returns based on the uuid that is the email
+//        return auth.currentUser?.email?.let {
+//            ref.document(it).get().await().toObject(User::class.java)
+//        }
+//    }
 
+    suspend fun getCurrentUser(): User? {
+        val email = auth.currentUser?.email
+        var docId = "testId"
+        val query = ref.whereEqualTo("email", email).get().await()
+        query.documents.forEach {
+            docId = it.id
+        }
+        return ref.document(docId).get().await().toObject(User::class.java)
+    }
 }
--- app/src/main/java/com/fantasy/fantasyfootball/repository/UserRepository.kt
+++ app/src/main/java/com/fantasy/fantasyfootball/repository/UserRepository.kt
@@ -5,16 +5,6 @@ import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
-    suspend fun getUserById(userId: Int): Flow<User?>
-
-    suspend fun getUserByUsername(username: String): User?
-
-    suspend fun getUserCredentials(username: String, password: String): User?
-
-    suspend fun createUser(user: User): Long
-
-    suspend fun editUser(id: String, user: User): Long
-
     suspend fun updateUser(id: String, user: User)
 
     suspend fun register(user: User): FirebaseUser?
