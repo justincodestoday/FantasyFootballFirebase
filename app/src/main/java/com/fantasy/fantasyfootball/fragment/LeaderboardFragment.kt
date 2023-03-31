219ab4de8d56d8cb085e981c2138092be90e1cf2
juju <jcmc888@gmail.com>
2023-03-31 14:47:58 +0800

    added loader

--- app/src/main/java/com/fantasy/fantasyfootball/fragment/LeaderboardFragment.kt
+++ app/src/main/java/com/fantasy/fantasyfootball/fragment/LeaderboardFragment.kt
@@ -3,12 +3,14 @@ package com.fantasy.fantasyfootball.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fantasy.fantasyfootball.R
 import com.fantasy.fantasyfootball.adapter.LeaderboardAdapter
 import com.fantasy.fantasyfootball.databinding.FragmentLeaderboardBinding
 import com.fantasy.fantasyfootball.viewModel.LeaderboardViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LeaderboardFragment : BaseFragment<FragmentLeaderboardBinding>() {
@@ -19,18 +21,29 @@ class LeaderboardFragment : BaseFragment<FragmentLeaderboardBinding>() {
     override fun onBindView(view: View, savedInstanceState: Bundle?) {
         super.onBindView(view, savedInstanceState)
 
-        viewModel.getUsers()
-
         setupAdapter()
 
-        viewModel.users.observe(viewLifecycleOwner) {users ->
+//        binding?.swiperefresh?.setOnRefreshListener {
+//            viewModel.getUsers()
+//            binding?.swiperefresh?.isRefreshing = false
+//        }
+    }
+
+    override fun onBindData(view: View) {
+        super.onBindData(view)
+
+        viewModel.users.observe(viewLifecycleOwner) { users ->
             adapter.setLeaderboard(users)
-            binding?.progress?.visibility = View.GONE
         }
 
-        binding?.swiperefresh?.setOnRefreshListener {
-            viewModel.getUsers()
-            binding?.swiperefresh?.isRefreshing = false
+        lifecycleScope.launch {
+            viewModel.isLoading.collect {
+                if (it) {
+                    binding?.progress?.visibility = View.VISIBLE
+                } else {
+                    binding?.progress?.visibility = View.GONE
+                }
+            }
         }
     }
 
